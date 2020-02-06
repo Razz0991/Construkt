069794526135cddf0ffc5adf296394e5b349c926
Razz0991 <paul.dav.1991@gmail.com>
2020-02-06 15:57:41 +1000

    Removed old loop incrementer (forgot)

--- src/main/java/com/github/razz0991/construkt/shapes/BaseShape.java
+++ src/main/java/com/github/razz0991/construkt/shapes/BaseShape.java
@@ -13,22 +13,6 @@ import com.github.razz0991.construkt.CktUtil;
 public abstract class BaseShape {
 	public abstract boolean generateShape(Location[] locations, boolean placeInAir, BlockData blockData);
 	
-	public void incrementLoop(int[] from, int[] to, int[] increment, int[] current) {
-		if (from.length != to.length && 
-				from.length != increment.length && 
-				from.length != current.length)
-			return;
-		
-		current[current.length - 1] += increment[current.length - 1];
-		
-		for (int i = current.length -1; i >= 0; i--) {
-			if (current[i] == to[i] + increment[i]) {
-				current[i-1] += increment[i-1];
-				current[i] = from[i];
-			}
-		}
-	}
-	
 	public void setBlock(BlockData blockData, Location loc) {
 		if (blockData != null)
 			loc.getBlock().setBlockData(blockData.clone());
