1467faf8e89c6fb7c96ef93a40aa2b5c176c131e
Razz0991 <paul.dav.1991@gmail.com>
2020-03-16 20:58:53 +1000

    Updated javadoc

--- src/main/java/com/github/razz0991/construkt/protection/ProtectionPlugins.java
+++ src/main/java/com/github/razz0991/construkt/protection/ProtectionPlugins.java
@@ -15,10 +15,21 @@ public class ProtectionPlugins {
 	
 	private static List<ProtectionBase> protectionPlugins = new ArrayList<ProtectionBase>();
 	
+	/**
+	 * Adds a protection plugin to check building against.
+	 * @param plugin The {@link ProtectionBase} to add
+	 */
 	public static void addProtectionPlugin(ProtectionBase plugin) {
 		protectionPlugins.add(plugin);
 	}
 	
+	/**
+	 * Checks if a player can build in a location. This checks all protection
+	 * plugins that have been added.
+	 * @param loc The {@link Location} to test
+	 * @param ply The {@link Player} placing the block
+	 * @return true if the player can place the block
+	 */
 	public static boolean canBuild(Location loc, Player ply) {
 		for (ProtectionBase plugin : protectionPlugins) {
 			if (!plugin.canBuild(loc, ply))
